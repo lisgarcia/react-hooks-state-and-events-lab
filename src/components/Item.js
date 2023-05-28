a2696da301e920f09077cee5b3afcdd1089f6764
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-27 22:08:06 -0400

    almost

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(true)
+
+  function handleClick () {
+    setInCart(!inCart)
+  }
   return (
-    <li className="">
+    <li className= { inCart ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick = {handleClick} className="add">{ inCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('')
+  
+  function handleFilter (e) {
+    setSelectedCategory(e.target.value)
+    //setting the selectedCategory to the clicked item
+    //need to take selectedCategory and have that be the updated list shown. 
+  }
+
+  const filteredCategories = items.filter((items) => {
+    // return ({selectedCategory}.includes(item.category))
+    return items.category.includes({selectedCategory})
+    
+    
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleFilter} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
